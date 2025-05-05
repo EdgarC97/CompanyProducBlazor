30ffd79ef259a9c231f129f746156f35930b3cb1
EdgarC97 <ecardona742@gmail.com>
2025-05-05 15:33:48 -0500

    feat: create interfaces for services

---
+++ Services/Interfaces/ICompanyService.cs
@@ -0,0 +1,13 @@
+﻿using CompanyProductBlazor.Models;
+
+namespace CompanyProductBlazor.Services.Interfaces
+{
+    public interface ICompanyService
+    {
+        Task<IEnumerable<Company>> GetAllCompaniesAsync();
+        Task<Company> GetCompanyByIdAsync(int id);
+        Task<Company> CreateCompanyAsync(Company company);
+        Task<Company> UpdateCompanyAsync(int id, Company company);
+        Task DeleteCompanyAsync(int id);
+    }
+}
---
+++ Services/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using CompanyProductBlazor.Models;
+
+namespace CompanyProductBlazor.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<IEnumerable<Product>> GetProductsByCompanyIdAsync(int companyId);
+        Task<Product> GetProductByIdAsync(int id);
+        Task<Product> CreateProductAsync(Product product);
+        Task<Product> UpdateProductAsync(int id, Product product);
+        Task DeleteProductAsync(int id);
+    }
+}
