b551ef72f4e3e7756df888ccc948c2695bfe26e9
EdgarC97 <ecardona742@gmail.com>
2025-05-05 15:31:51 -0500

    feat: update models

---
+++ Models/Company.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CompanyProductBlazor.Models
+
+{
+    public class Company
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "La dirección es obligatoria")]
+        [StringLength(200, ErrorMessage = "La dirección no puede exceder los 200 caracteres")]
+        public string Address { get; set; }
+
+        public List<Product> Products { get; set; } = new List<Product>();
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanyProductBlazor.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria")]
+        [StringLength(500, ErrorMessage = "La descripción no puede exceder los 500 caracteres")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "El precio es obligatorio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor que 0")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "La compañía es obligatoria")]
+        public int CompanyId { get; set; }
+
+        public Company Company { get; set; }
+    }
+}
